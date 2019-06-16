52b663bb5c84c3d9722d075b24cecb9e0557bc9d
omar sagoo <om.sagoo@gmail.com>
2019-06-15 17:18:59 -0700

    fixed no location error

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -5,19 +5,21 @@ const geocode = (address, callback) => {
         '.json?access_token=pk.eyJ1Ijoib21zYWdvbyIsImEiOiJjandqa3Jzd2cwNmk1NDltcGtndnBlejJlIn0.UKJXHSBKz8_TRjzwVQrudw&limit=1'
 
     request({ url, json: true }, (error, {body}) => {
+        const length = body.features.length
 
-        const { length, center, place_name } = body.features[0]
-
+        if (!length) {
+            return callback('Unable to find Location! Try another search!', undefined)
+        }
+        const { center, place_name } = body.features[0]
         if (error) {
             callback('Unable to connect to location services!', undefined)
-        } else if (length === 0) {
-            callback('Unable to find Location! Try another search!', undefined)
         } else {
             callback(undefined, {
                 latitude: center[1],
                 longitude: center[0],
                 location: place_name
             })
+        
         }
     })
 }
